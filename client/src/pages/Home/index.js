bf96e695a9e32985c18e54318ba985a7623cb012
Vontell <sankarsh.rajeev@gmail.com>
2022-12-12 18:30:23 -0800

    files added

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,14 +1,14 @@
 function Home() {
 	return (
 		<form>
-			<div className="mb-3">
-				<label for="exampleInputEmail1" className="form-label">Email address</label>
-				<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-					<div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+			<div className="mb-3 form-group">
+				<label>Title</label>
+				<input type="email" className="form-control" placeholder="Enter the Title" />
+					
 			</div>
 			<div className="mb-3">
-				<label for="exampleInputPassword1" className="form-label">Password</label>
-				<input type="password" className="form-control" id="exampleInputPassword1"/>
+				<label>Detials</label>
+				<input type="text" className="form-control" id="exampleInputPassword1"/>
 			</div>
 			<button type="submit" className="btn btn-primary">Submit</button>
 		</form>
