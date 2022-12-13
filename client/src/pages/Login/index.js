0b6e7eeaeffd9135fd0595f85e61e55678803e5e
Armando.Rod <armando.rod79@gmail.com>
2022-12-12 20:30:58 -0800

    fixed Login Css

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -44,79 +44,52 @@ const Login = (props) => {
 
   return (
     <main className="flex-row justify-center mb-4">
-
-      <div class="wrapper">
+<div class="wrapper">
         <div class="logo">
-          <img src="https://www.freepnglogos.com/uploads/farmer-png/farmer-man-vector-cartoon-characters-ultimate-packs-31.png" alt="Farmer" />
+            <img src="https://www.freepnglogos.com/uploads/farmer-png/farmer-man-vector-cartoon-characters-ultimate-packs-31.png" alt="Farmer"/>
         </div>
         <div class="text-center mt-4 name">
-          Farmer-To-Do
+            Farmer-To-Do
         </div>
-        {/* <form onSubmit={handleFormSubmit}/> */}
-        <div class="form-field d-flex align-items-center">
-          <span class="far fa-user"></span>
-
-
-
-
-          <div className="col-12 col-lg-10">
-            <div className="card">
-              <h4 className="card-header bg-dark text-light p-2">Login</h4>
-              <div className="card-body">
-                {data ? (
-                  <p>
-                    Success! You may now head{' '}
-                    <Link to="/">back to the homepage.</Link>
-                  </p>
-                ) : (
-                  <form onSubmit={handleFormSubmit}>
-                    <input
-                      className="form-input"
-                      placeholder="Your email"
-                      name="email"
-                      type="email"
-                      value={formState.email}
-                      onChange={handleChange}
-                    />
-                    <input
-                      className="form-input"
-                      placeholder="******"
-                      name="password"
-                      type="password"
-                      value={formState.password}
-                      onChange={handleChange}
-                    />
-                    <button
-                      className="btn btn-block btn-info"
-                      style={{ cursor: 'pointer' }}
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </form>
-                )}
-                </div>
-        </div>
-
-                {error && (
-                  <div className="my-3 p-3 bg-danger text-white">
-                    {error.message}
-                  </div>
-                )}
-              </div>
+        <form onSubmit={handleFormSubmit}>
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input
+                  className="form-input"
+                  placeholder="Your email"
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleChange}
+                />
             </div>
-          
-      </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input
+                  className="form-input"
+                  placeholder="Password"
+                  // id="pwd"
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+            </div>
+            <button class="btn mt-3">Login</button>
+        </form>
+        <div class="text-center fs-6">
+            <a href="#">Forget password?</a> or <a href="./SignUp">Sign up</a>
+        </div>
+                    {error && (
+              <div className=" rounded border border-dark my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+    </div>
     </main>
+    
   );
 };
 
+
 export default Login;
-
-
-
-
-
-
-
-
