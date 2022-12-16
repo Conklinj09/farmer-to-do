adb37d25d2ad46fe3211795de7d5ffcc25b800c6
Lexi M <104409081+Babylex710@users.noreply.github.com>
2022-12-15 20:24:31 -0800

    added Todo.css

--- client/src/components/Todo/Todo.js
+++ client/src/components/Todo/Todo.js
@@ -4,6 +4,7 @@ import { GET_TODOS } from '../../utils/queries';
 import AddTodos from '../../components/Todo/AddTodos';
 import { useMutation } from '@apollo/client';
 import { DELETE_TODO } from '../../utils/mutations';
+import './Todo.css';
 
 const Todo = ({id,title,date,detail}) => {
     const [deleteTodo] = useMutation(DELETE_TODO);
@@ -17,7 +18,7 @@ const Todo = ({id,title,date,detail}) => {
         })
     }
     return(
-        <a href="#" className="list-group-item list-group-item-action " aria-current="true">
+        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
 					<div className="d-flex w-100 justify-content-between">
 						<h5 className="mb-1">{title}</h5>
 						<small>{moment(date).format("MMMM DD YYYY")}</small>
